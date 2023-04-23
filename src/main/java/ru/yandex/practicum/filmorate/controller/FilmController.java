8e37f98f94f07e92e285feeb0dd020c3d4ef2d68
SergeyZhirov47 <SylarWesker@yandex.ru>
2023-04-23 22:58:06 +0300

    Добавлены контроллеры для пользователей и фильмов

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -0,0 +1,59 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import javax.validation.Valid;
+import java.security.InvalidParameterException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestController
+@RequestMapping("/films")
+public class FilmController {
+    private final Map<Integer, Film> films = new HashMap<>();
+    private int lastId = 0;
+    private int nextId() {
+        lastId++;
+        return lastId;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Film add(@Valid @RequestBody Film newFilm) {
+        final int newId = nextId();
+        newFilm.setId(newId);
+
+        films.put(newId, newFilm);
+        log.info(String.format("Добавлен новый фильм %s", newFilm.getName()));
+
+        return newFilm;
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Film update(@Valid @RequestBody Film film) {
+        final int filmId = film.getId();
+        if (films.containsKey(filmId)) {
+            films.put(filmId, film);
+        } else {
+            throw new NotFoundException(String.format("Нет фильма с id = %s. Обновление не успешно.", filmId));
+        }
+
+        log.info(String.format("Обновлена информация о фильме с id %s", filmId));
+
+        return film;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Film> getAll() {
+        return films.values().stream().collect(Collectors.toUnmodifiableList());
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,63 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final Map<Integer, User> users = new HashMap<>();
+    private int lastId = 0;
+    private int nextId() {
+        lastId++;
+        return lastId;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User create(@Valid @RequestBody User newUser) {
+        if (StringUtils.isBlank(newUser.getName())) {
+           newUser.setName(newUser.getLogin());
+        }
+
+        final int newId = nextId();
+        newUser.setId(newId);
+        users.put(newId, newUser);
+
+        log.info(String.format("Добавлен новый пользователь {0}", newUser));
+
+        return newUser;
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    public User update(@Valid @RequestBody User user) {
+        final int userId = user.getId();
+        if (users.containsKey(userId)) {
+            users.put(userId, user);
+        } else {
+            throw new NotFoundException(String.format("Нет пользователя с id = %s. Обновление не успешно.", userId));
+        }
+
+        log.info(String.format("Обновлена информация о пользователе с id %s", userId));
+
+        return user;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getUsers() {
+        return users.values().stream().collect(Collectors.toUnmodifiableList());
+    }
+}
