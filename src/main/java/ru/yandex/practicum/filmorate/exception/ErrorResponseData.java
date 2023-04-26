38e5b80bae3ac4a80c0d42576e21a4bb10ef2b0e
SergeyZhirov47 <SylarWesker@yandex.ru>
2023-04-26 16:43:13 +0300

    refactor: убрана лишняя аннотация @Getter так как его добавит lomboc при использовании @Data

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorResponseData.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorResponseData.java
@@ -1,10 +1,8 @@
 package ru.yandex.practicum.filmorate.exception;
 
 import lombok.Data;
-import lombok.Getter;
 
 @Data
 public class ErrorResponseData {
-    @Getter
     private final String message;
 }
