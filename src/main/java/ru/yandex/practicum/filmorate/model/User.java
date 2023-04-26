eca8edade4c73601c54e827860e7dd711c42e9cb
SergeyZhirov47 <SylarWesker@yandex.ru>
2023-04-26 16:49:35 +0300

    fix: заменил @Past на @PastOrPresent у для рождения потому что так правильно по тз

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Past;
+import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 
 @Data
@@ -15,6 +15,6 @@ public class User {
     @NotBlank(message = "Логин не может быть пустым")
     private String login;
     private String name;
-    @Past(message = "Дата дня рождения не может быть в будущем")
+    @PastOrPresent(message = "Дата дня рождения не может быть в будущем")
     private LocalDate birthday;
 }
