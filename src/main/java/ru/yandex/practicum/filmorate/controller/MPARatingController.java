37b9efe2b233e26f8b0cb8a8b403af9e10eda0ce
SergeyZhirov47 <SylarWesker@yandex.ru>
2023-06-05 18:51:50 +0300

    refactor: убраны не нужные аннотации для статуса 200 в контроллерах.

--- src/main/java/ru/yandex/practicum/filmorate/controller/MPARatingController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MPARatingController.java
@@ -2,8 +2,10 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.model.MPA;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
@@ -17,14 +19,12 @@ public class MPARatingController {
     private final FilmService filmService;
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
     public List<MPA> getAll() {
         log.info("GET /mpa");
         return filmService.getAllMPARatings();
     }
 
     @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
     public MPA getMPARatingById(@PathVariable(name = "id") int id) {
         log.info(String.format("GET /mpa/{id}, {id} = %s", id));
         return filmService.getMPARating(id);
