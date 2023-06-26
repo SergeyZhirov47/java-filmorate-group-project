c4e1d3e679e63e8e09daa37feb77e386b0145f9b
AnastasiaKolycheva <tainstvennaya-dama@yandex.ru>
2023-06-26 19:07:02 +0300

    fix: Исправить замечания

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/UserDbStorage.java
@@ -8,7 +8,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
